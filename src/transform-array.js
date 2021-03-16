22d0bcfe2e702d1e4a5d6c84ef10733ba1312dee
YoursTruly450 <yourstruly.bill@gmail.com>
2021-03-16 22:42:45 +0400

    feat: 10/10 tests passed

--- src/transform-array.js
+++ src/transform-array.js
@@ -1,6 +1,49 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function transform(/* arr */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function transform(arr) {
+  const isItem = (el) => !String(el).startsWith('--');
+  if (Array.isArray(arr)) {
+    let array = [];
+    let buffArray = [];
+    let discard = false;
+    for (let i = 0; i < arr.length; i++) {
+      if (!discard) {
+        buffArray.push(arr[i]);
+        switch (arr[i]) {
+          case '--discard-next':
+            if (!discard) {
+              discard = !discard;
+            }
+            break;
+          case '--discard-prev':
+            if (isItem(buffArray[i - 1]) && array.length > 0) {
+              array.splice(array.length - 1, 1);
+            }
+            break;
+          case '--double-next':
+            if (i < arr.length - 1 && isItem(arr[i + 1])) {
+              let item = arr[i + 1];
+              array.push(item);
+            }
+            break;
+          case '--double-prev':
+            if (isItem(buffArray[i - 1]) && array.length > 0) {
+              let item = array[array.length - 1];
+              array.push(item);
+            }
+            break;
+          default:
+            let item = arr[i];
+            array.push(item);
+            break;
+        }
+      } else {
+        buffArray.push('--discard');
+        discard = !discard;
+      }
+    }
+    return array;
+  } else {
+    throw new Error();
+  }
 };
--- src/what-season.js
+++ src/what-season.js
@@ -1,6 +1,22 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function getSeason(/* date */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function getSeason(date) {
+  if (!date) {
+    return 'Unable to determine the time of year!';
+  }
+  if (date instanceof Date && !isNaN(date.valueOf())) {
+    if (date.getMonth() <= 1) {
+      return 'winter';
+    } else if (date.getMonth() <= 4) {
+      return 'spring';
+    } else if (date.getMonth() <= 7) {
+      return 'summer';
+    } else if (date.getMonth() <= 10) {
+      return 'autumn';
+    } else {
+      return 'winter';
+    }
+  } else {
+    throw new Error();
+  }
 };
